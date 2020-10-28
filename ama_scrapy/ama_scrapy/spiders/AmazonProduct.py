262c51aaef830094004536d10e182ee23075d081
山岡智裕 <>
2020-10-29 08:35:45 +0900

    ADD:amazonスパイダーの作成

--- ama_scrapy/ama_scrapy/spiders/AmazonProduct.py
+++ ama_scrapy/ama_scrapy/spiders/AmazonProduct.py
@@ -1,13 +1,32 @@
 import scrapy
-
+from ..items import 
 
 class AmazonproductSpider(scrapy.Spider):
     name = 'AmazonProduct'
     allowed_domains = ['amazon.co.jp']
     start_urls = ['http://amazon.co.jp/']
 
+    #クロールを開始するURLを動的に変えたい
+    def start_request(self):
+        url = 'https://sardine-system.com/media/'
+        yield scrapy.Request(url, callback=self.parse)
+    
     def parse(self, response):
-        pass
-
+        """
+        レスポンスに対するパース処理
+        """
+        for post in response.css():
+            yield  Post(
+                url=
+            )
+     # 再帰的にページングを辿るための処理
+        older_post_link = response.css('.blog-pagination a.next-posts-link::attr(href)').extract_first()
+        if older_post_link is None:
+     # リンクが取得できなかった場合は最後のページなので処理を終了
+            return
+     # URLが相対パスだった場合に絶対パスに変換する
+        older_post_link = response.urljoin(older_post_link)
+        # 次のページをのリクエストを実行する
+        yield scrapy.Request(older_post_link, callback=self.parse)
 
 #https://qiita.com/Chanmoro/items/f4df85eb73b18d902739
